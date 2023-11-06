471a10a78dab56c55bf81da850c77a00e749f9e1
Gigi-U <andanlibres@gmail.com>
2023-11-06 17:14:47 -0300

    web server Hello Workd

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Gigi-U/primera_api_go_web_server.git/internal/webserver"
+)
+
+func main(){
+		fmt.Println("\n----------Gin web framework---------------")
+	webserver.WebServer()
+
+
+}
+
+/* 
+Gin es un microframework de alto rendimiento que se puede utilizar para crear aplicaciones web y
+microservicios en Go.
+Este contiene un conjunto de funcionalidades (por ejemplo: routing, middleware, rendering, etc.) que reducen el código repetitivo y simplifican la creación de aplicaciones web y microservicios. 
+
+1) crear repositorio
+2) clonar repositorio
+3) go mod init github.com/MiUsuario/nombreDeMiRepo
+4) go get -u github.com/gin-gonic/gin
+
+PRUEBA TERMINAL =
+
+{"message":"Hello World!"}
+
+
+
+*/
