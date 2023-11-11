ba9e80388d69a327e86f6f3494db35fe7f9fb655
Gigi-U <andanlibres@gmail.com>
2023-11-11 20:03:39 -0300

    agregando readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # primera_api_go_web_server
+
+ 
+Gin es un microframework de alto rendimiento que se puede utilizar para crear aplicaciones web y microservicios en Go.
+
+Este contiene un conjunto de funcionalidades (por ejemplo: routing, middleware, rendering, etc.) que reducen el código repetitivo y simplifican la creación de aplicaciones web y microservicios. 
+
+    1) crear repositorio
+    2) clonar repositorio
+    3) go mod init github.com/MiUsuario/nombreDeMiRepo
+    4) go get -u github.com/gin-gonic/gin
+
+
--- main.go
+++ main.go
@@ -16,23 +16,13 @@ func main(){
 	mt.ObtenerProductos()
 	fmt.Println("\n----------CLASE 10---------------")
 	professor.GetProducto()
+
+	
 	fmt.Println("\n----------CLASE 11---------------")
+
+
 	fmt.Println("\n----------CLASE 12---------------")
 	fmt.Println("\n----------CLASE 13---------------")
 
-
-
 }
 
-/* 
-Gin es un microframework de alto rendimiento que se puede utilizar para crear aplicaciones web y
-microservicios en Go.
-Este contiene un conjunto de funcionalidades (por ejemplo: routing, middleware, rendering, etc.) que reducen el código repetitivo y simplifican la creación de aplicaciones web y microservicios. 
-
-1) crear repositorio
-2) clonar repositorio
-3) go mod init github.com/MiUsuario/nombreDeMiRepo
-4) go get -u github.com/gin-gonic/gin
-
-
-*/
